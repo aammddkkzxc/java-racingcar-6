3693cb4c011aeacf50ffc93f0d10f6df1dbbf871
aammddkkzxc <ideal0427@naver.com>
2023-12-08 15:32:15 +0900

    refactor: 자동차 이름 입력 받는 기능 리턴타입 리팩터링

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -4,8 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String CAR_NAME_REQUEST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    public static String readCarNames() {
+    private static Cars readCarNames() {
         System.out.println(CAR_NAME_REQUEST_MESSAGE);
-        return Console.readLine();
+
+        String carNames = Console.readLine();
+        Cars cars = new Cars(Converter.convertCarNames(Console.readLine()));
+
+        return cars;
     }
 }
