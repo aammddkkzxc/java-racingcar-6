edce1a928ff74317ea04d63c016438f16c76e00d
aammddkkzxc <ideal0427@naver.com>
2023-12-08 15:29:47 +0900

    feat: 쉼표로 자동차 이름 구분 짓는 기능 구현

--- src/main/java/racingcar/Converter.java
+++ src/main/java/racingcar/Converter.java
@@ -1,4 +1,19 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Converter {
+    public static String CAR_NAME_DELIMITER = ",";
+
+    public static List<Car> convertCarNames(String carNames) {
+        String[] separatedCarNames = carNames.split(CAR_NAME_DELIMITER);
+        List<Car> cars = new ArrayList<>();
+
+        for(String carName : separatedCarNames) {
+            cars.add(new Car(carName));
+        }
+
+        return cars;
+    }
 }
