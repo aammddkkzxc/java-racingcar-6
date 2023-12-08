ef29f2f4ce82e6e0800bb33a5ae4b5841847b19e
aammddkkzxc <ideal0427@naver.com>
2023-12-08 15:38:50 +0900

    feat: 자동차 이름 5자 초과일 경우 예외 발생 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,10 +2,18 @@ package racingcar;
 
 public class Car {
     public static String CAR_NAMES_RE_REQUEST_MESSAGE = "자동차 이름을 다시 입력해 주십시오.";
+    private static final int CAN_NAME_LENGTH_MAXIMUM_LIMIT = 5;
 
     private final String name;
 
     public Car(String name) {
+        validateCarNameLength(name);
         this.name = name;
+    }
+
+    private void validateCarNameLength(String name) {
+        if (name.length() > CAN_NAME_LENGTH_MAXIMUM_LIMIT) {
+            throw new IllegalArgumentException(CAR_NAMES_RE_REQUEST_MESSAGE);
+        }
     }
 }
