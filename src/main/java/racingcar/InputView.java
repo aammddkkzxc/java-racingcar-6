d46595f31cde7d6eb0298131be9bbd0ac51aa214
aammddkkzxc <ideal0427@naver.com>
2023-12-08 15:49:27 +0900

    feat: 시도 회수 입력 기능 구현

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String CAR_NAME_REQUEST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ROUND_NUMBER_REQUEST_MESSAGE = "시도할 회수는 몇회인가요?";
 
     public static Cars InputCars() {
         try {
@@ -21,5 +22,14 @@ public class InputView {
         Cars cars = new Cars(Converter.convertCarNames(Console.readLine()));
 
         return cars;
+    }
+
+    private static Round readRound() {
+        System.out.println(ROUND_NUMBER_REQUEST_MESSAGE);
+
+        String round = Console.readLine();
+        Round round = ;
+
+        return round;
     }
 }
