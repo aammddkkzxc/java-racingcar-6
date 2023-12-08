7121ecf22dbc4b23f3f505f22b8d18cf7f69a393
aammddkkzxc <ideal0427@naver.com>
2023-12-08 15:35:46 +0900

    feat: 자동차 이름 입력 과정에서 예외 발생 시 재입력 받는 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,6 +1,8 @@
 package racingcar;
 
 public class Car {
+    public static String CAR_NAMES_RE_REQUEST_MESSAGE = "자동차 이름을 다시 입력해 주십시오.";
+
     private final String name;
 
     public Car(String name) {
--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -4,6 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String CAR_NAME_REQUEST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
+    public static Cars InputCars() {
+        try {
+            return readCarNames();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return InputCars();
+        }
+    }
+
     private static Cars readCarNames() {
         System.out.println(CAR_NAME_REQUEST_MESSAGE);
 
